aca6f4459ebd6ede982242594e05aa0b576f84a6
Jenny Gao <jenny20gz@gmail.com>
2020-06-10 17:14:21 +0000

    added user input feature to cafe map

---
+++ portfolio/src/main/java/com/google/sps/servlets/MapMarkerServlet.java
@@ -0,0 +1,87 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.sps.data.MapMarker;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.logging.Logger;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+@WebServlet("/mapmarkers")
+public class MapMarkerServlet extends HttpServlet {
+  private static final Gson gson = new Gson();
+  private static Logger log = Logger.getLogger("MapMarkerServlet");
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+
+    Collection<MapMarker> mapMarkers = getMapMarkers();
+    String json = gson.toJson(mapMarkers);
+    response.getWriter().println(json);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    String itemName = Jsoup.clean(request.getParameter("itemName"), Whitelist.none());
+    double lat = Double.parseDouble(request.getParameter("lat"));
+    double lng = Double.parseDouble(request.getParameter("lng"));
+    String content = Jsoup.clean(request.getParameter("content"), Whitelist.none());
+
+    MapMarker mapMarker = new MapMarker(itemName, lat, lng, content);
+    storeMapMarker(mapMarker);
+  }
+
+  private Collection<MapMarker> getMapMarkers() {
+    Collection<MapMarker> mapMarkers = new ArrayList<>();
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query mapMarkerQuery = new Query("MapMarker");
+    PreparedQuery results = datastore.prepare(mapMarkerQuery);
+
+    for (Entity entity : results.asIterable()) {
+      String itemName = (String) entity.getProperty("itemName");
+      double lat = (double) entity.getProperty("lat");
+      double lng = (double) entity.getProperty("lng");
+      String content = (String) entity.getProperty("content");
+
+      MapMarker mapMarker = new MapMarker(itemName, lat, lng, content);
+      mapMarkers.add(mapMarker);
+    }
+    return mapMarkers;
+  }
+
+  public void storeMapMarker(MapMarker mapMarker) {
+    Entity mapMarkerEntity = new Entity("MapMarker");
+    mapMarkerEntity.setProperty("itemName", mapMarker.getItemName());
+    mapMarkerEntity.setProperty("lat", mapMarker.getLat());
+    mapMarkerEntity.setProperty("lng", mapMarker.getLng());
+    mapMarkerEntity.setProperty("content", mapMarker.getContent());
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(mapMarkerEntity);
+  }
+}
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//let cafeMap;
+let editMarker;
+
 function randomizeRecipe() {
   // there are 4 recipes total
   const imageIndex = Math.floor(Math.random() * 4) + 1;
@@ -56,12 +59,12 @@ function createCommentsElement(comment) {
   const commentElement = document.createElement("li");
   commentElement.className = "comment";
 
-  const contentElement = document.createElement('span');
+  const contentElement = document.createElement("span");
   contentElement.innerText = comment.content;
 
-  const deleteButtonElement = document.createElement('button');
-  deleteButtonElement.innerText = 'Delete';
-  deleteButtonElement.addEventListener('click', () => {
+  const deleteButtonElement = document.createElement("button");
+  deleteButtonElement.innerText = "Delete";
+  deleteButtonElement.addEventListener("click", () => {
     deleteComment(comment);
 
     commentElement.remove();
@@ -74,37 +77,99 @@ function createCommentsElement(comment) {
 
 function deleteComment(comment) {
   const params = new URLSearchParams();
-  params.append('id', comment.id);
-  fetch('/delete-comment', {method: 'POST', body: params});
+  params.append("id", comment.id);
+  fetch("/delete-comment", {method: "POST", body: params});
 }
 
 function loadMap() {
   const sanFrancisco = {lat: 37.7749, lng: -122.4194};
-  const cafeMap = new google.maps.Map(
+  cafeMap = new google.maps.Map(
     document.getElementById("map"),
     {center: sanFrancisco, zoom: 12});
 
-  loadMapItem(cafeMap, "Philz Coffee", 37.793949, -122.398062, "I like the Iced Coffee Rose!");
-  loadMapItem(cafeMap, "Saint Frank Coffee", 37.779511, -122.410411, 
+  cafeMap.addListener("click", (event) => {
+    createMapMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+  });
+
+  fetchMapMarkers();
+  loadMapItem("Philz Coffee", 37.793949, -122.398062, "I like the Iced Coffee Rose!");
+  loadMapItem("Philz Coffee", 37.793949, -122.398062, "I like the Iced Coffee Rose!");
+  loadMapItem("Saint Frank Coffee", 37.779511, -122.410411, 
               "The hot chocolate & cappuccino are superb");
-  loadMapItem(cafeMap, "Stonemill Matcha", 37.764730, -122.421731, "I like the yuzu meringue");
-  loadMapItem(cafeMap, "Four Barrel Coffee", 37.768055, -122.422117, "The lattes are spectacular!");
+  loadMapItem("Stonemill Matcha", 37.764730, -122.421731, "I like the yuzu meringue");
+  loadMapItem("Four Barrel Coffee", 37.768055, -122.422117, "The lattes are spectacular!");
 }
 
-function loadMapItem(mapName, itemName, latValue, lngValue, itemDescription) {
+async function fetchMapMarkers() {
+  const response = await fetch("/mapmarkers");
+  const mapMarkers = await response.json();
+  mapMarkers.forEach((mapMarker) => {
+    loadMapItem(mapMarker.itemName, mapMarker.lat, mapMarker.lng, mapMarker.content)
+  });
+}
+
+function loadMapItem(itemName, latitudeValue, longitudeValue, itemDescription) {
   const itemMarker = new google.maps.Marker({
-    position: {lat:	latValue, lng: lngValue},
-    map: mapName, 
+    position: {lat:latitudeValue, lng: longitudeValue},
+    map: cafeMap,
     title: itemName
   });
 
   const itemInfoWindow = new google.maps.InfoWindow({
-   // content: itemDescription
- 
     content: "<h1>"+itemName+"</h1>"+"<p>"+itemDescription+"</p>"
   });
 
-  itemMarker.addListener('click', function(){
-    itemInfoWindow.open(mapName, itemMarker);
+  itemMarker.addListener("click", function(){
+    itemInfoWindow.open(cafeMap, itemMarker);
   });
+}
+
+function postMapMarker(itemName, lat, lng, content) {
+  const params = new URLSearchParams();
+  params.append("itemName", itemName);
+  params.append("lat", lat);
+  params.append("lng", lng);
+  params.append("content", content);
+
+  fetch("/mapmarkers", {method:"POST", body:params});
+}
+
+function createMapMarkerForEdit(lat, lng){
+  // remove editable marker if one is already shown
+  if (editMarker) {
+    editMarker.setMap(null);
+  }
+
+  editMarker = new google.maps.Marker({position: {lat: lat, lng: lng}, map: cafeMap});
+
+  const infoWindow = new google.maps.InfoWindow({content: buildInfoWindowInput(lat, lng)});
+
+  google.maps.event.addListener(infoWindow, "closeclick", () => {
+      editMarker.setMap(null);
+  });
+
+  console.log(editMarker);
+  infoWindow.open(cafeMap, editMarker);
+}
+
+function buildInfoWindowInput(lat, lng) {
+  const titleBox = document.createElement("textarea");
+  const descriptionBox = document.createElement("textarea");
+  const submitButton = document.createElement("button");
+  submitButton.appendChild(document.createTextNode("Submit"));
+
+  submitButton.onclick = () => {
+    postMapMarker(titleBox.value, lat, lng, descriptionBox.value);
+    loadMapItem(titleBox.value, lat, lng, descriptionBox.value);
+    editMarker.setMap(null);
+  };
+
+  const entryBox = document.createElement("div");
+  entryBox.appendChild(titleBox);
+  entryBox.appendChild(document.createElement("br"));
+  entryBox.appendChild(descriptionBox);
+  entryBox.appendChild(document.createElement("br"));
+  entryBox.appendChild(submitButton);
+
+  return entryBox;
 }
